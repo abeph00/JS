1702eb6319a7304624301fb4a5658360c1f61845
abeph <abeph@Germen>
2023-01-23 14:25:16 +0100

    listo

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer((request, response) => {
+  let filename = "index.html";
+  let contentType = "text/html";
+
+  if(request.url === "/style.css") {
+    filename = "style.css";
+    contentType = "text/css";
+  }
+  fs.readFile(filename, function(err, content) {
+    if(err) {
+        response.statusCode = 500;
+        response.end(`Could not serve ${filename}`);
+    }
+    else {
+        response.statusCode = 200;
+        response.setHeader('Content-Type', contentType);
+        response.end(content);
+    }
+  });
+});
+server.listen({ port: 3000, host: 'localhost' }, () => {
+  console.log('Up and Running!');
+});
